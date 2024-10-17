91de34910306c6d8048c67aee19996c3317307ae
Abdalla-Elhagar <134546609+Abdalla-Elhagar@users.noreply.github.com>
2024-10-17 21:18:29 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //  Exercise 1
 
-let arr = [1,2,3,4,5,6,7,8,9,10]
+let arr1 = [1,2,3,4,5,6,7,8,9,10]
 
 function evenNums(arr){
     return arr.filter(e=> e%2 == 0)
 }
 
-console.log(evenNums(arr))
+console.log(evenNums(arr1))
 
 
 
@@ -14,13 +14,13 @@ console.log(evenNums(arr))
 
 //  Exercise 2
 
-let arr = [1,2,3,4,5,6,207,8,9,10]
+let arr2 = [1,2,3,4,5,6,207,8,9,10]
 
 function largestNum(arr){
     return Math.max(...arr)
 }
 
-console.log(largestNum(arr))
+console.log(largestNum(arr2))
 
 
 //  Exercise 3
@@ -35,11 +35,11 @@ console.log(reverseString(text))
 
 // Exercise 4
 
-let arr = [1,2,3,5,5,8,9,8,7,1]
+let arr3 = [1,2,3,5,5,8,9,8,7,1]
 
 function deleteTheDuplicate(arr) {
     newArr = arr.filter((Element,Index) => arr.indexOf(Element) === Index)
     return newArr
 }
 
-console.log(deleteTheDuplicate(arr))
+console.log(deleteTheDuplicate(arr3))
