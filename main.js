1c3083eb41b789fab0f0ab96dbdbe8ba421e609b
Abdalla-Elhagar <134546609+Abdalla-Elhagar@users.noreply.github.com>
2024-10-17 21:08:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //  Exercise 1
 
-// let arr = [1,2,3,4,5,6,7,8,9,10]
+let arr = [1,2,3,4,5,6,7,8,9,10]
 
-// function evenNums(arr){
-//     return arr.filter(e=> e%2 == 0)
-// }
+function evenNums(arr){
+    return arr.filter(e=> e%2 == 0)
+}
 
-// console.log(evenNums(arr))
+console.log(evenNums(arr))
 
 
 
@@ -14,32 +14,32 @@
 
 //  Exercise 2
 
-// let arr = [1,2,3,4,5,6,207,8,9,10]
+let arr = [1,2,3,4,5,6,207,8,9,10]
 
-// function largestNum(arr){
-//     return Math.max(...arr)
-// }
+function largestNum(arr){
+    return Math.max(...arr)
+}
 
-// console.log(largestNum(arr))
+console.log(largestNum(arr))
 
 
 //  Exercise 3
 
-// let text = "Code Masters";
-// function reverseString(text) {
-//     return text.split('').reverse().join('')
-// }
+let text = "Code Masters";
+function reverseString(text) {
+    return text.split('').reverse().join('')
+}
 
-// console.log(reverseString(text))
+console.log(reverseString(text))
 
 
 // Exercise 4
 
-// let arr = [1,2,3,5,5,8,9,8,7,1]
+let arr = [1,2,3,5,5,8,9,8,7,1]
 
-// function deleteTheDuplicate(arr) {
-//     newArr = arr.filter((Element,Index) => arr.indexOf(Element) === Index)
-//     return newArr
-// }
+function deleteTheDuplicate(arr) {
+    newArr = arr.filter((Element,Index) => arr.indexOf(Element) === Index)
+    return newArr
+}
 
-// console.log(deleteTheDuplicate(arr))
+console.log(deleteTheDuplicate(arr))
